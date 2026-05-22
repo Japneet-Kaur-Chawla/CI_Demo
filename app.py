ebce0a9e2aeb3ebc23765871ca22ce525b2ab1b9
Japneet-KaurChawla <japneetkaurchawla231106@gmail.com>
2026-05-22 20:13:24 +0530

    small change

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ def add(a, b):
 
 
 def sub(a, b):
-    return a - b
+    return a-b
 
 
-def multi(a, b):
-    return a * b
+def mul(a, b):
+    return a*b
 
-result1 = add(4, 5)
-result2 = sub(4, 1)
 
-print(result1, result2)
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(4, 1)
+
+    print(result1, result2)
